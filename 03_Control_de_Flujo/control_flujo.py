d93dabf713afe4113069d4c32b7c8f596b242b6b
Victor Hernandez <vmhm182@gmail.com>
2024-08-15 15:28:48 -0600

    Ciclo For, While, Actualizacion de README

--- 03_Control_de_Flujo/control_flujo.py
+++ 03_Control_de_Flujo/control_flujo.py
@@ -8,3 +8,14 @@ elif edad == 18:
     print("Acabas de Cumplir la mayoria de edad")
 else:
     print("Ya eres un adulto")
+    
+# Ejemplo del bucle `for`
+
+for i in range(5):
+    print("Numero: ", i)
+    
+# Ejemplo del bucle `while`
+contador = 0
+while contador < 5:
+    print("Contador: ",contador)
+    contador += 1
