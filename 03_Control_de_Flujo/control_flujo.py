a00aa5dc9bf7777101d854a0ff0bb1b4da2a523e
Victor Hernandez <vmhm182@gmail.com>
2024-08-15 15:08:12 -0600

    Control de flujos, condicionales y archivo README

---
+++ 03_Control_de_Flujo/control_flujo.py
@@ -0,0 +1,10 @@
+# Ejemplo de condicionales
+
+edad = 89
+
+if edad < 18:
+    print("Eres menor de edad")
+elif edad == 18:
+    print("Acabas de Cumplir la mayoria de edad")
+else:
+    print("Ya eres un adulto")
