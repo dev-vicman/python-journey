e14d6ce07ba5317ed9e9e991069f05acd057ef46
Victor Hernandez <vmhm182@gmail.com>
2024-08-16 14:51:31 -0600

    Inicio de leccion de estrutura de datos

---
+++ 05_Estructuras_de_Datos/estructuras_de_datos.py
@@ -0,0 +1,17 @@
+# Definición de una lista
+frutas = ["manzana", "banana", "cereza"]
+
+# Acceso a elementos
+print(frutas[0])  # Output: manzana
+
+# Modificación de elementos
+frutas[1] = "pera"
+
+# Añadir elementos
+frutas.append("naranja")
+
+# Remover elementos
+frutas.remove("cereza")
+
+print(frutas)
+
